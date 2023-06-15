eb61689c4f858c6b63f0995e1dbee8439a04bab6
ivanroscuenca <ivanros@protonmail.com>
2023-06-15 19:42:50 +0200

    services done

--- crudBACK/src/main/java/com/tutorial/crud/security/service/UserDetailsServiceImpl.java
+++ crudBACK/src/main/java/com/tutorial/crud/security/service/UserDetailsServiceImpl.java
@@ -1,4 +1,22 @@
 package com.tutorial.crud.security.service;
 
-public class UserDetailsServiceImpl {
+import com.tutorial.crud.security.entity.Usuario;
+import com.tutorial.crud.security.entity.UsuarioPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+//implementamos interface UserDetailsService
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    //inyectamos usuarioService
+    @Autowired
+    UsuarioService usuarioService;
+    @Override
+    public UserDetails loadUserByUsername(String nombreUsuario) throws UsernameNotFoundException {
+       Usuario usuario = usuarioService.getByNombreUsuario(nombreUsuario).get();
+       return UsuarioPrincipal.build(usuario);
+    }
 }
