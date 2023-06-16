2e5bf548713554bf6a8e1a14ba84d2094afb07f1
ivanroscuenca <ivanros@protonmail.com>
2023-06-16 13:58:07 +0200

    Dto Class done

--- crudBACK/src/main/java/com/tutorial/crud/security/dto/NuevoUsuario.java
+++ crudBACK/src/main/java/com/tutorial/crud/security/dto/NuevoUsuario.java
@@ -1,4 +1,65 @@
 package com.tutorial.crud.security.dto;
 
+import com.sun.istack.NotNull;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
+
 public class NuevoUsuario {
+
+    @NotBlank
+    private String nombre;
+    @NotBlank
+    private String nombreUsuario;
+    @Email
+    private String email;
+    @NotBlank
+    private String password;
+    //Hasset tipo String porque ha de ser JSON
+
+    private Set<String> roles = new HashSet<>();
+
+//Geters and setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
 }
